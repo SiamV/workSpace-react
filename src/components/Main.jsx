3a4597702dcb3a9deae3713df536dfbcd9c02327
SiamV <avers3452@gmail.com>
2022-08-03 10:53:51 -0500

    clean project for new

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,20 +1,9 @@
-import React, { useState } from "react";
-import ChangeDate from "./ChangeDate";
-import NewCB from "./NewCB";
+import React from 'react'
 
 const Main = () => {
-  const [list, setList] = useState("Fist Date");
-    let newDate = (date) => {
-        setList(date)
-    }
   return (
-    <div>
-      <h1>Main</h1>
-      <div>'Date from localState:'{list}</div>
-      <ChangeDate setNewDateCB={newDate}/>
-      <NewCB setNewCB={setList} />
-    </div>
-  );
-};
+    <div>Main</div>
+  )
+}
 
-export default Main;
+export default Main
