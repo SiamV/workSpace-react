fec45194dc939249d3f752d4a4f75a2ac2daa7d5
SiamV <avers3452@gmail.com>
2022-08-05 12:25:50 -0500

    css is done

--- src/components/CityInfo.jsx
+++ src/components/CityInfo.jsx
@@ -1,20 +1,24 @@
 import React from "react";
+import Preloader from "./utiles/Preloader/Preloader.js";
+import classes from "./cityInfo.module.css"
 
 const CityInfo = ({ data }) => {
   return (
     <div>
       {data !== null ? (
-        <div>
-          <div>{data.data.location.name}</div>
+        <div className={classes.cityInfoContainer}>
+          <div className={classes.cityName}>{data.data.location.name}</div>
           <div>
             <img
               src={`${data.data.current.condition.icon}`}
               alt={data.data.current.condition.text}
             />
           </div>
-          <div>{data.data.current.temp_c}</div>
+          <div className={classes.cityTemp}>{data.data.current.temp_c}</div>
         </div>
-      ) : null}
+      ) : (
+        <Preloader />
+      )}
     </div>
   );
 };
