272594c3af28ed47fa2a73d8c1997cf3f50d58c5
SiamV <avers3452@gmail.com>
2022-08-10 10:47:34 -0500

    clean app for new

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,42 +1,8 @@
-import React, { useState, useEffect } from "react";
-import CityInfo from "./CityInfo";
-import axios from 'axios';
+import React from "react";
 
 const Main = () => {
-  const [city, setCity] = useState("");
-  const [cityName, setCityName] = useState("Mexico");
-  const [data, setData] = useState(null);
-
-  let onPushEnter = (e) => {
-    if (e.key === "Enter") {
-      setCityName(city);
-    }
-  };
-  useEffect(() => {
-      try {
-        axios
-          .get(
-            `https://api.weatherapi.com/v1/current.json?key=dc73c66b52484a6baf024943220508&q=${cityName}&aqi=no`
-          )
-          .then((response) => {
-            setData(response);
-          });
-      } catch (e) {
-        console.log(e);
-      }
-  }, [cityName]);
-
   return (
-    <div>
-      <div>Widget Clima</div>
-      <input
-        onChange={(e) => {
-          setCity(e.target.value);
-        }}
-        value={city}
-        onKeyDown={onPushEnter}
-      />
-      <CityInfo data={data} />
+    <div> Hello
     </div>
   );
 };
