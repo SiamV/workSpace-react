aeb6791a7e406499aa3f83a72184b8c283fb58e0
SiamV <avers3452@gmail.com>
2022-08-29 10:48:52 -0500

    create main Modal.jsx

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Modal from './components/Modal/Modal';
 import NavBar from './components/NavBar';
 import ProjectSerach from './components/ProjectSearch/ProjectSerach';
 
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route exact path={"/"} element={<NavBar />}/>
         <Route path={"/project-search"} element={<ProjectSerach />}/>
+        <Route path={"/modal"} element={<Modal/>}/>
       </Routes>
       </BrowserRouter>
     </div>
