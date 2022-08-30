1eae32b7e646f05fab0e195c80068090e4c53c78
SiamV <avers3452@gmail.com>
2022-08-30 13:52:29 -0500

    Lazy loading in App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
+import React from 'react';
+import { Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import NavBar from './components/NavBar';
-import ProjectSerach from './components/ProjectSearch/ProjectSerach';
+import Preloader from './components/utiles/Preloader/Preloader.js'
+
+const NavBar = React.lazy(() => import('./components/NavBar'));
+const ProjectSerach = React.lazy(() => import('./components/ProjectSearch/ProjectSerach'));
+
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Routes>
-        <Route exact path={"/"} element={<NavBar />}/>
-        <Route path={"/project-search"} element={<ProjectSerach />}/>
-      </Routes>
+        <Routes>
+          <Route exact path={"/"} element={<NavBar />} />
+          <Suspense fallback={<Preloader />}>
+            <Route path={"/project-search"} element={<ProjectSerach />} />
+          </Suspense>
+        </Routes>
       </BrowserRouter>
     </div>
   );
